5bab0af05fb8db01c8e166273ab9ade6cc218ce1
RUPAM DAS <132121805+rupamdas2003@users.noreply.github.com>
2026-05-26 11:23:59 +0530

    Add files via upload

---
+++ pages/post/[postId].js
@@ -0,0 +1,136 @@
+import { getSession, withPageAuthRequired } from "@auth0/nextjs-auth0";
+import { AppLayout } from "../../components/appLayout/AppLayout";
+import clientPromise from "../../lib/mongodb";
+import { ObjectId } from "mongodb";
+import Markdown from "react-markdown";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHashtag } from "@fortawesome/free-solid-svg-icons";
+import { getAppProps } from "../../utils/getAppProps";
+import { useContext, useState } from "react";
+import { useRouter } from "next/router";
+import PostContext from "../../context/postContext";
+
+export default function Post(props) {
+  const [showDeleteConfirm, setShowDeleteConfirm] = useState(false);
+  const router = useRouter();
+  const { deletePost } = useContext(PostContext);
+
+  const handleDeleteConfirm = async () => {
+    try {
+      const response = await fetch("/api/deletePost", {
+        method: "POST",
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify({ postId: props.id }),
+      });
+
+      const data = await response.json();
+
+      if (data.success) {
+        deletePost(props.id);
+        router.replace("/post/new");
+      }
+    } catch (error) {}
+  };
+
+  return (
+    <div className="overflow-auto h-full">
+      <div className="max-w-screen-sm mx-auto">
+        <div className="text-sm font-bold mt-6 p-2 bg-stone-200 rounded-sm">
+          SEO title and meta description
+        </div>
+        <div className="p-4 my-2 border border-stone-200 rounded-md">
+          <div className="text-blue-600 text-2xl font-bold">{props.title}</div>
+          <div className="mt-2">{props.metaData}</div>
+        </div>
+        <div className="text-sm font-bold mt-6 p-2 bg-stone-200 rounded-sm">
+          Keywords
+        </div>
+        <div className="flex flex-wrap pt-2 gap-1">
+          {props.keywords.split(",").map((keyword, i) => (
+            <div key={i} className="p-2 rounded-full bg-slate-800 text-white">
+              <FontAwesomeIcon icon={faHashtag} />
+              {keyword}
+            </div>
+          ))}
+        </div>
+        <div className="text-sm font-bold mt-6 p-2 bg-stone-200 rounded-sm">
+          Blog Post
+        </div>
+        <Markdown>{props.postContent || ""}</Markdown>
+        <div className="my-4">
+          {!showDeleteConfirm && (
+            <button
+              className="btn bg-red-600 hover:bg-red-700"
+              onClick={() => setShowDeleteConfirm(true)}
+            >
+              Delete post
+            </button>
+          )}
+          {!!showDeleteConfirm && (
+            <div className="p-2 bg-red-200 text-center">
+              <p>
+                Are you sure you want to delete this post? This action is
+                irreversible
+              </p>
+              <div className="grid grid-cols-2 gap-2">
+                <button
+                  className="btn bg-stone-600 hover:bg-stone-700"
+                  onClick={() => setShowDeleteConfirm(false)}
+                >
+                  Cancel
+                </button>
+                <button
+                  className="btn bg-red-600 hover:bg-red-700"
+                  onClick={handleDeleteConfirm}
+                >
+                  Confirm
+                </button>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+Post.getLayout = function getLayout(page, pageProps) {
+  return <AppLayout {...pageProps}>{page}</AppLayout>;
+};
+
+export const getServerSideProps = withPageAuthRequired({
+  async getServerSideProps(ctx) {
+    const props = await getAppProps(ctx);
+    const userSession = await getSession(ctx.req, ctx.res);
+    const client = await clientPromise;
+    const db = client.db("blog-ai");
+    const user = await db.collection("users").findOne({
+      auth0Id: userSession.user.sub,
+    });
+    const post = await db.collection("posts").findOne({
+      _id: new ObjectId(ctx.params.postId),
+      userId: user._id,
+    });
+
+    if (!post) {
+      return {
+        redirect: {
+          destination: "/post/new",
+          permanent: false,
+        },
+      };
+    }
+
+    return {
+      props: {
+        id: ctx.params.postId,
+        postContent: post.postContent,
+        title: post.title,
+        metaData: post.metaData,
+        keywords: post.keywords,
+        postCreated: post.created.toString(),
+        ...props,
+      },
+    };
+  },
+});
